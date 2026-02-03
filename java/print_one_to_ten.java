f679694424675c35f0661c85dc27ce6573c0daef
ankurgupta1436 <wwwankurgupta824@gmail.com>
2026-02-03 22:01:12 +0530

    Number 1-10

---
+++ java/print_one_to_ten.java
@@ -0,0 +1,8 @@
+// to print 1 -10 using for loop
+public class print_one_to_ten {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
